0db06e45cefc4a60bb537a1bbf2fcbfb866ed542
Nitin <nkapania@gmail.com>
2018-06-15 18:43:00 -0700

    Adding PySim - my framework for vehicle dynamics simulations

---
+++ newTest.m
@@ -0,0 +1,8 @@
+close all; clear all; clc;
+addpath(genpath('common'))
+load '/home/nkapania/Desktop/research/simulation/common/maps/thunderhill_race.mat'
+path = mapWorld; path.isOpen = 0;
+
+veh = getVehicle('nonlinear','embed');
+
+%%
