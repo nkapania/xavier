5c89b5b51e1f9f6523dd5c56e5915919530f15f4
Nitin <nkapania@gmail.com>
2018-06-19 16:40:31 -0700

    Building in simulation framework - roughly 40% complete

--- newTest.m
+++ newTest.m
@@ -4,8 +4,11 @@ load '/home/nkapania/Desktop/research/simulation/common/maps/simpleRace.mat'
 path = world; path.isOpen = 0;
 
 veh = getVehicle('nonlinear','embed');
-%plot(veh.alphaFrontTable, veh.FyFtable)
 vp = generateSpeedTrajectory(path, veh);
+
+ts = .01;
+[sim, lapTime] = bikeSim(path, veh, ts, vp);
+
 
 %plot(vp.s, vp.Ux)
 
