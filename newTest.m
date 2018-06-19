19c7001fb5612c7376395ccd80ee76887569b0cb
Nitin <nkapania@gmail.com>
2018-06-19 12:05:34 -0700

    Implemented velocity profile

--- newTest.m
+++ newTest.m
@@ -1,9 +1,12 @@
 close all; clear all; clc;
 addpath(genpath('common'))
-load '/home/nkapania/Desktop/research/simulation/common/maps/thunderhill_race.mat'
-path = mapWorld; path.isOpen = 0;
+load '/home/nkapania/Desktop/research/simulation/common/maps/simpleRace.mat'
+path = world; path.isOpen = 0;
 
 veh = getVehicle('nonlinear','embed');
-plot(veh.alphaFrontTable, veh.FyFtable)
+%plot(veh.alphaFrontTable, veh.FyFtable)
+vp = generateSpeedTrajectory(path, veh);
+
+%plot(vp.s, vp.Ux)
 
 %%
