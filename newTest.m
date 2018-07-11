f56b9f7e28d978ee6724d886c313e089a4ce0410
Nitin <nkapania@gmail.com>
2018-07-11 13:00:41 -0700

    embedded map matching now works in python

--- newTest.m
+++ newTest.m
@@ -3,7 +3,7 @@ addpath(genpath('common'))
 load '~/research/simulation/common/maps/simpleRace.mat'
 path = world; path.isOpen = 0;
 
-veh = getVehicle('nonlinear','euler');
+veh = getVehicle('nonlinear','closest');
 vp = generateSpeedTrajectory(path, veh);
 
 ts = .01;
